c580370e016c05f32bb9c5b802f133bf6d3e1527
TicTacToe_sst <paliwalshreshtha@gmail.com>
2024-04-24 22:33:31 +0530

    TicTacToe

---
+++ src/controllers/GameController.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import exceptions.InvalidMoveException;
+import models.Game;
+import models.GameState;
+import models.Player;
+
+import java.util.List;
+
+public class GameController {
+    public Game startGame(int dimension, List<Player> players){
+        //validate if 2 players have the same symbol or not?
+        //if 2 players have same symbol, throw some exception.
+        return new Game(dimension, players);
+    }
+
+    public void makeMove(Game game) throws InvalidMoveException {
+        game.makeMove();
+    }
+
+    public GameState checkState(Game game){
+        return game.getGameState();
+    }
+
+    public Player getWinner(Game game){
+        return game.getWinner();
+    }
+
+    public void printBoard(Game game){
+        game.printBoard();
+    }
+}
